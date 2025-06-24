9c9171d25d41b9443174d1e13e2e567ff81655cb
Augusto Hagemeier <augustolhagemeier@gmail.com>
2025-06-24 13:09:04 -0300

    Inicio trabalho final

---
+++ TrabalhoImobiliaria/Modelo/Imovel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    internal class Imovel
+    {
+        public int Id { get; set; }
+        public int NroQuartos { get; set; }
+        public int NroVagas { get; set; }
+        public int NroBanheiros { get; set; }
+        public Address? Address { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public Categoria? Categoria { get; set; }
+    }
+}
